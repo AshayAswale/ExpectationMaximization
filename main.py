96949751843b7e809bdc587b9db996e3560bff99
Abhishek Jain <33022116+AJ1897@users.noreply.github.com>
2020-04-01 03:16:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,155 +1,86 @@
-import csv
-import sys
 import numpy as np
-import random
-from copy import deepcopy
-from numpy import genfromtxt
-from scipy import linalg
-# from scipy.spatial import distance
-from matplotlib import pyplot
-import math
+from time import time
+import sys
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from em import EM
 import time
 
-
-def plotPoints(points, means, init_centroids, num=1):
-    pyplot.scatter(points[:, 0], points[:, 1], c=points[:, 2], s=2)
-    for i in range(len(means)):
-        pyplot.scatter(means[i, 0], means[i, 1], c=i, marker="*", s=90)
-        pyplot.scatter(init_centroids[i, 0], init_centroids[i, 1], c=4)
-    # pyplot.show()
-    name = 'EM_animation_3/books_read_'
-    save_name = name + str(num) + '.png'
-    print(save_name)
-    pyplot.savefig(save_name)
-    pyplot.close()
-
-
 def main(argv):
-    points = genfromtxt(argv[0], delimiter=",")
-    solve(points, int(argv[1]))
+    # Reading and storing data
+    my_file=open(argv[0])
+    file_lines=my_file.readlines()
+    # No of Clusters
+    noc=int(argv[1])
+    coordinates=[]
+    random_restarts=10
+    for i in range(len(file_lines)):
+        file_lines[i]=file_lines[i].replace("\n","")
+        file_lines[i]=file_lines[i].split(" ")
+        coordinates.append([])
+        for j in range(len(file_lines[i])):
+            coordinates[i].append(float(file_lines[i][j]));
+    # No of Dimensions
+    n=len(coordinates[0])
+    # Solver
+    start_time=time.time()
+    z=0  
+    while(z<random_restarts and time.time()-start_time<50):
+        cluster=EM(n,coordinates,noc)
+        for i in range(20):
+            # e= Expected Value.shape=(len(data),noc)
+            e=cluster.expectation(coordinates)
+            # Mean= Covariance Matrix=[Mean cluster1, Mean cluster2, Mean cluster3]
+            mean=np.array(cluster.mean(e,coordinates))
+            # Matrix= Covariance Matrix=[cov cluster1, cov cluster2, cov cluster3]
+            matrix=cluster.covariance(e,coordinates,mean)
+            # Cont= Prob(cluster)
+            cont=np.array(sum(e))/len(coordinates)
+            cl=[]
+            for t in range(noc):
+                cl.append([])
+            for q in range(len(coordinates)):
+                for w in range(noc):
+                    if e[q][w]==e[q].max():
+                        cl[w].append(coordinates[q])
+            print(cluster.log_likelihood(coordinates,e))
+            # Update Values
+            for j in range(noc):
+                print(mean[j],"\n")
+                cluster.clusters["cluster %s"%(j+1)]["mean"]=mean[j]
+                cluster.clusters["cluster %s"%(j+1)]["cov"]=matrix[j]
+                cluster.clusters["cluster %s"%(j+1)]["cont"]=cont[j]
 
-    # # # Delete when done
-    # N = 100
-    # points = np.random.rand(N, 1)
-    # solve(points, 1)
-
-
-def solve(points, n_clust):
-    points_dimension = len(points[0])
-
-    # Append a row of zeros.
-    temp = np.zeros((len(points), points_dimension + 1))
-    temp[:, :-1] = points
-    points = temp
-
-    # Centroid with n-dimention and variance
-    # means[ith centroid][x-cord, y-cord, std devn]
-    means = np.zeros((n_clust, points_dimension))
-    for i in range(n_clust):
-        means[i] = random.choice(points[:, :-1])
-
-    # Should be random symmetric matrix
-    covariances = np.zeros((n_clust, points_dimension, points_dimension))
-    for i in range(n_clust):
-        covariances[i] = np.identity(points_dimension)
-
-    # means = np.array([[20, 70, 5], [15, -10, 5], [-30, 15, 5]])
-
-    expectationMaximisation(points, means, covariances)
-
-
-def expectationMaximisation(points, means, covariances):
-    p_centroid = np.full((len(means)), 1 / (len(means)))
-    init_centroids = deepcopy(means)
-    start_time = time.time()
-    elapsed_time = time.time() - start_time
-    i = 0
-    # while elapsed_time < 10:
-    while i < 10:
-        likelihood = getLikelihood(points, means, covariances, p_centroid)
-        print(likelihood)
-        p_x_cl_arr = getGaussianProbArray(points, means, covariances)
-        cl_i_array = getProbOfBelonging(p_x_cl_arr, p_centroid)
-        assignClusters(cl_i_array, points)
-        # print(cl_i_array)
-        updateCentroids(points, means, covariances)
-        elapsed_time = time.time() - start_time
-        i += 1
-        # print(i)
-        plotPoints(points, means, init_centroids, i)
-    print(i)
-    print(means)
-
-
-def getLikelihood(points, means, covariances, p_centroid):
-    likelihood = 0
-    for i in range(len(points)):
-        temp = 0
-        for j in range(len(means)):
-            temp += (p_centroid[j]) * getGaussianProb(
-                points[i], means[j], covariances[j])
-        likelihood += np.log(temp)
-    return likelihood
+            # Plot Data 
+            fig = plt.figure()
+            ax = plt.axes()
+            ax.scatter((np.array(cl[0]).T)[0],(np.array(cl[0]).T)[1])
+            ax.scatter((np.array(cl[1]).T)[0],(np.array(cl[1]).T)[1])
+            ax.scatter((np.array(cl[2]).T)[0],(np.array(cl[2]).T)[1])
+            # ax.scatter((np.array(cl[3]).T)[0],(np.array(cl[3]).T)[1])
+            # ax.scatter((np.array(cl[4]).T)[0],(np.array(cl[4]).T)[1])
+            # ax.scatter((np.array(coordinates).T)[0],(np.array(coordinates).T)[1])
+            ax.scatter(mean.T[0],mean.T[1],c=[0,1,2])
+            ax.set_xlabel("X")
+            ax.set_ylabel("Y")
 
 
-def assignClusters(cl_i_array, points):
-    for i in range(len(cl_i_array)):
-        points[i, -1] = np.argmax(cl_i_array[i])
+            plt.show() 
+        
+            # plt.close(fig)
+        z+=1
+        print("\n")        
+
+    # print(cluster.clusters)
+    # print("\n")
+    # print(np.array(e))
+    # print(mean)
+    # print(matrix,"\n")
+
+    # print(cluster.clusters)
+    # print(mean)
 
 
-def updateCentroids(points, means, covariances):
-    sums = np.zeros(len(means))
-    loc_means = deepcopy(means)
-    means.fill(0)
-    # covariances.fill(0)
-    for i in range(len(points)):
-        cluster = int(points[i, -1])
-        sums[cluster] += 1
-        means[cluster] += points[i, :-1]
-        # covariances[cluster] += np.dot(np.atleast_2d(
-        #     points[i, :-1] - loc_means[i]).T, (points[i, :-1] - loc_means[i]))
-        diff = np.atleast_2d(points[i, :-1] - loc_means[cluster])
-        covariances[cluster] += np.dot(diff.T, diff)
-
-    for i in range(len(means)):
-        means[i] /= sums[i]
-        covariances[i] /= sums[i]
-
-    i = 0
-
-
-def getProbOfBelonging(p_x_cl_arr, p_centroid):
-    for point in p_x_cl_arr:
-        sum = 0
-        for i in range(len(point)):
-            sum += point[i] * p_centroid[i]
-            point[i] *= p_centroid[i]
-        if sum == 0.0:
-            point[:] = 1/len(p_centroid)
-        else:
-            point[:] /= sum
-    return p_x_cl_arr
-
-
-def getGaussianProbArray(points, means, covariances):
-    probabilities = np.zeros((len(points), len(means)))
-    for i in range(len(points)):
-        for j in range(len(means)):
-            probabilities[i, j] = getGaussianProb(
-                points[i], means[j], covariances[j])
-    return probabilities
-
-
-def getGaussianProb(point, centroid, covariance):
-    dimention = len(centroid)
-    denom = (1 / (((2 * math.pi) ** (dimention / 2))
-                  * (abs(np.linalg.det(covariance)) ** 0.5)))
-    diff = np.atleast_2d(point[:-1]-centroid)
-    expon = -1/2 * \
-        (np.dot(np.dot(diff, (np.linalg.inv(covariance))), ((diff).T)))
-
-    # return expon + np.log(denom)
-    return denom*np.exp(expon)
 
 
 if __name__ == "__main__":
