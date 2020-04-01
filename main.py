e81d935b698bba7e8a36adc9f9b76a6de2987c63
Ashay Aswale <asaswale@wpi.edu>
2020-03-31 22:12:48 -0400

    WIP: NAN problem, Only covers one iteration

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import numpy as np
 import random
 from copy import deepcopy
 from numpy import genfromtxt
-from scipy.spatial import distance
+from scipy import linalg
+# from scipy.spatial import distance
 from matplotlib import pyplot
 import math
 import time
 
 
-def plotPoints(points, centroids, init_centroids, num=1):
+def plotPoints(points, means, init_centroids, num=1):
     pyplot.scatter(points[:, 0], points[:, 1], c=points[:, 2], s=2)
-    for i in range(len(centroids)):
-        pyplot.scatter(centroids[i, 0], centroids[i, 1], c=i, marker="*", s=90)
+    for i in range(len(means)):
+        pyplot.scatter(means[i, 0], means[i, 1], c=i, marker="*", s=90)
         # pyplot.scatter(init_centroids[i, 0], init_centroids[i, 1], c=4)
     # pyplot.show()
-    name = 'EM_animation_2/books_read_'
+    name = 'EM_animation_3/books_read_'
     save_name = name + str(num) + '.png'
     print(save_name)
     pyplot.savefig(save_name)
@@ -42,35 +43,40 @@ def solve(points, n_clust):
     points = temp
 
     # Centroid with n-dimention and variance
-    # centroids[ith centroid][x-cord, y-cord, std devn]
-    centroids = np.zeros((n_clust, points_dimension + 1))
+    # means[ith centroid][x-cord, y-cord, std devn]
+    means = np.zeros((n_clust, points_dimension))
     for i in range(n_clust):
-        centroids[i] = random.choice(points)
-    centroids[:, -1] = 3
+        means[i] = random.choice(points[:, :-1])
 
-    # centroids = np.array([[20, 70, 5], [15, -10, 5], [-30, 15, 5]])
+    # Should be random symmetric matrix
+    covariances = np.zeros((n_clust, points_dimension, points_dimension))
+    for i in range(n_clust):
+        covariances[i] = np.identity(points_dimension)
 
-    expectationMaximisation(points, centroids)
+    # means = np.array([[20, 70, 5], [15, -10, 5], [-30, 15, 5]])
+
+    expectationMaximisation(points, means, covariances)
 
 
-def expectationMaximisation(points, centroids):
-    p_centroid = np.full((len(centroids)), 1 / (len(centroids)))
-    init_centroids = deepcopy(centroids)
+def expectationMaximisation(points, means, covariances):
+    p_centroid = np.full((len(means)), 1 / (len(means)))
+    init_centroids = deepcopy(means)
     start_time = time.time()
     elapsed_time = time.time() - start_time
     i = 0
-    while elapsed_time < 10:
-        # while i < 20:
-        p_x_cl_arr = getGaussianProbArray(points, centroids)
+    # while elapsed_time < 10:
+    while i < 10:
+        p_x_cl_arr = getGaussianProbArray(points, means, covariances)
         cl_i_array = getProbOfBelonging(p_x_cl_arr, p_centroid)
-        updateCentroids(points, cl_i_array, centroids)
+        assignClusters(cl_i_array, points)
+        print(cl_i_array)
+        updateCentroids(points, means, covariances)
         elapsed_time = time.time() - start_time
         i += 1
-        assignClusters(cl_i_array, points)
         print(i)
-        plotPoints(points, centroids, init_centroids, i)
+        plotPoints(points, means, init_centroids, i)
     print(i)
-    print(centroids)
+    print(means)
 
 
 def assignClusters(cl_i_array, points):
@@ -78,19 +84,24 @@ def assignClusters(cl_i_array, points):
         points[i, -1] = np.argmax(cl_i_array[i])
 
 
-def updateCentroids(points, cl_i_array, centroids):
-    for i in range(len(centroids)):
-        sum = 0
-        mean = np.zeros((len(centroids[0]) - 1))
-        variance = 0
-        for j in range(len(points)):
-            sum += cl_i_array[j, i]
-            for k in range(len(points[0])-1):
-                mean[k] += cl_i_array[j, i] * points[j, k]
-            variance += (cl_i_array[j, i] *
-                         ((getDistance(points[j, :-1], centroids[i, :-1]))**2))
-        centroids[i, :-1] = mean / sum
-        centroids[i, -1] = (variance/sum)**0.5
+def updateCentroids(points, means, covariances):
+    sums = np.zeros(len(means))
+    loc_means = deepcopy(means)
+    means.fill(0)
+    covariances.fill(0)
+    for i in range(len(points)):
+        cluster = int(points[i, -1])
+        sums[cluster] += 1
+        means[cluster] += points[i, :-1]
+        # covariances[cluster] += np.dot(np.atleast_2d(points[i, :-1] - loc_means[i]).T, (points[i, :-1] -loc_means[i]))
+        diff = np.atleast_2d(points[i, :-1] - loc_means[cluster])
+        covariances[cluster] += np.dot(diff.T, diff)
+
+    for i in range(len(means)):
+        means[i] /= sums[i]
+        covariances /= sums[i]
+
+    i = 0
 
 
 def getProbOfBelonging(p_x_cl_arr, p_centroid):
@@ -100,27 +111,26 @@ def getProbOfBelonging(p_x_cl_arr, p_centroid):
             sum += point[i] * p_centroid[i]
             point[i] *= p_centroid[i]
         point[:] /= sum
-
     return p_x_cl_arr
 
 
-def getGaussianProbArray(points, centroids):
-    probabilities = np.zeros((len(points), len(centroids)))
+def getGaussianProbArray(points, means, covariances):
+    probabilities = np.zeros((len(points), len(means)))
     for i in range(len(points)):
-        for j in range(len(centroids)):
-            probabilities[i, j] = getGaussianProb(points[i], centroids[j])
+        for j in range(len(means)):
+            probabilities[i, j] = getGaussianProb(
+                points[i], means[j], covariances[j])
     return probabilities
 
 
-def getGaussianProb(point, centroid):
-    denom = (1 / (((2 * math.pi) ** 0.5)*centroid[-1]))
-    expon = -((getDistance(point[:-1], centroid[:-1])
-               ** 2)) / (2 * (centroid[-1] ** 2))
-    return math.exp(expon)*denom
+def getGaussianProb(point, centroid, covariance):
+    dimention = len(centroid)
+    denom = (1 / (((2 * math.pi) ** (dimention / 2))
+                  * (abs(np.linalg.det(covariance)) ** 0.5)))
+    expon = -1/2*(np.dot(np.dot(np.atleast_2d(point[:-1]-centroid),
+                                (np.linalg.inv(covariance))), ((np.atleast_2d(point[:-1]-centroid)).T)))
 
-
-def getDistance(point, centroid):
-    return distance.euclidean(point, centroid)
+    return (expon + np.log(denom))
 
 
 if __name__ == "__main__":
